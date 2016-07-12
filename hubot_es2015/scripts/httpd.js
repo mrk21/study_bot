bba58439f816b8db5b52ee7c616d8c8bb9bd4abc
Yuichi Murata <mrk21info+github@gmail.com>
2016-07-12 23:59:07 +0900

    hubot_es2015: fix a code of httpd

--- hubot_es2015/scripts/httpd.js
+++ hubot_es2015/scripts/httpd.js
@@ -1,7 +1,8 @@
 export default robot => {
   // $ curl http://localhost:8080/endpoint_test
   // > hubot httpd test
-  robot.router.get('/endpoint_test', (req, res) =>
-    res.end('hubot httpd test')
-  );
+  robot.router.get('/endpoint_test', (req, res) => {
+    robot.messageRoom('hubot_test', 'received http request');
+    res.end('hubot httpd test');
+  });
 };
