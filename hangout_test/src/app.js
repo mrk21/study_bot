998baa92a8b71604dabc0c6c5b0afb83570650f9
Yuichi Murata <mrk21info+github@gmail.com>
2016-07-16 23:57:01 +0900

    hangout_test: add a code of participants event

--- hangout_test/src/app.js
+++ hangout_test/src/app.js
@@ -1,9 +1,15 @@
 function init() {
-  gapi.hangout.onApiReady.add(function(eventObj) {
-    if (eventObj.isApiReady) {
-      console.log('ready');
-    }
+  console.log('app init');
+
+  gapi.hangout.onParticipantsAdded.add(function (participants) {
+    console.log('onParticipantsAdded', participants);
   });
 }
 
-gadgets.util.registerOnLoadHandler(init);
+gadgets.util.registerOnLoadHandler(function () {
+  gapi.hangout.onApiReady.add(function (eventObj) {
+    if (eventObj.isApiReady) {
+      init();
+    }
+  });
+});
