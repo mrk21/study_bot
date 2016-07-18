725790e251ee3de1307122cfca66534f4fee6043
Yuichi Murata <mrk21info+github@gmail.com>
2016-07-18 23:53:57 +0900

    hangout_test: show participants

--- hangout_test/src/app.js
+++ hangout_test/src/app.js
@@ -1,9 +1,23 @@
+function showParticipants() {
+  var participants = gapi.hangout.getParticipants();
+  var ul = document.getElementById('participants');
+
+  participants.forEach(function (participant) {
+    var li = document.createElement('li');
+    console.log(participant.person.displayName);
+    li.innerHTML = participant.person.displayName;
+    ul.appendChild(li);
+  });
+}
+
 function init() {
   console.log('app init');
   console.log('getEnabledParticipants', gapi.hangout.getEnabledParticipants());
+  showParticipants();
 
   gapi.hangout.onParticipantsAdded.add(function (participants) {
     console.log('onParticipantsAdded', participants);
+    showParticipants();
   });
 }
 
